d4ccd4e90314eab89390da0c789cbfcd92bebd4f
Justin Bermudez <justinanbermudez@gmail.com>
2019-12-12 11:38:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
 }
